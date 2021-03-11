302b17673ff26d3b865000e12ca9fd204558b19f
Swapnil Dubey <swapnil.vishu.7027@gmail.com>
2021-03-12 01:25:26 +0530

    added emoji list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles.css";
 import { useState } from "react";
 
-const emojiDictionary = {
+var emojiDictionary = {
   "😀": "Smiling",
   "🤮": "Vomiting",
   "😪": "Sleepy",
   "🥱": "Yawning"
 };
+
+var emojiWeKnow = Object.keys(emojiDictionary);
 export default function App() {
   const [meaning, setMeaning] = useState("");
   function emojiInputHandler(event) {
@@ -19,11 +21,27 @@ export default function App() {
     }
     setMeaning(meaning);
   }
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <input onChange={emojiInputHandler} />
       <h2>{meaning}</h2>
+      <h3>Emoji We Know</h3>
+      {emojiWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
     </div>
   );
 }
