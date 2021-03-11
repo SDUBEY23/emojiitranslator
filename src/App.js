57203051f1db3ef5df58864f7f5d01c849af0508
Swapnil Dubey <swapnil.vishu.7027@gmail.com>
2021-03-12 00:44:55 +0530

    added meaning on search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import "./styles.css";
+import { useState } from "react";
 
+const emojiDictionary = {
+  "😀": "Smiling",
+  "🤮": "Vomiting",
+  "😪": "Sleepy",
+  "🥱": "Yawning"
+};
 export default function App() {
+  const [meaning, setMeaning] = useState("");
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+
+    var meaning = emojiDictionary[userInput];
+    //console.log(meaning);
+    if (meaning === undefined) {
+      meaning = "We don't have this in our bucket";
+    }
+    setMeaning(meaning);
+  }
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
+      <input onChange={emojiInputHandler} />
+      <h2>{meaning}</h2>
     </div>
   );
 }
